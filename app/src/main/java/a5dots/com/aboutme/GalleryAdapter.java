b80fc67e244969d76e616dfe521be7fee2e5ceee
Alex Scrobot <scrobot91@gmail.com>
2018-01-30 15:12:07 +0700

    init

---
+++ app/src/main/java/a5dots/com/aboutme/GalleryAdapter.java
@@ -0,0 +1,34 @@
+package a5dots.com.aboutme;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alexscrobot on 30.01.18.
+ */
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder> {
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
